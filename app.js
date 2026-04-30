afb765bbcf2f23ce4ce8dfe7158bdc26b4159565
Currodg <cdiagir@ieslucussolis.es>
2026-04-30 08:25:13 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const link1 = document.createElement('link');
 link1.rel = 'icon';
-link1.href = 'favicon.ico';
+link1.href = './favicon.ico';
 link1.type = 'image/x-icon';
 
 // Crear el segundo enlace para compatibilidad (shortcut icon)
 const link2 = document.createElement('link');
 link2.rel = 'shortcut icon';
-link2.href = 'favicon.ico';
+link2.href = './favicon.ico';
 link2.type = 'image/x-icon';
 
 // Añadirlos al <head> del documento
